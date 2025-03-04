21f3bf8a0d3648ed11b53ff0bfc91a354a901000
midodae13 <59099138+midodae13@users.noreply.github.com>
2025-03-04 09:16:15 +0100

    Add Marzipano Files

---
+++ app-files/data.js
@@ -0,0 +1,222 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r0010634",
+      "name": "R0010634",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.6545291624224792,
+        "pitch": 0.1548439929031442,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2516941639267039,
+          "pitch": 0.17459745879103394,
+          "rotation": 0,
+          "target": "1-r0010635"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.4216349690644279,
+          "pitch": 0.24598571589963214,
+          "title": "Trude",
+          "text": "<span style=\"color: rgb(17, 17, 17); font-family: -apple-system, Roboto, SegoeUI, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Microsoft YaHei&quot;, &quot;Meiryo UI&quot;, Meiryo, &quot;Arial Unicode MS&quot;, sans-serif; font-size: 14px; background-color: rgb(243, 243, 243);\">Trude ist das heimliche Genie des Bauernhofs. Mit ihrem scharfsinnigen Blick und ihrer Vorliebe für komplexe Denksportaufgaben könnte sie locker bei jeder Quizshow gewinnen – wenn sie denn sprechen könnte. Stattdessen nutzt sie ihre Intelligenz, um immer die besten Weideplätze zu finden und die leckersten Grashalme zu ergattern.</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-r0010635",
+      "name": "R0010635",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-r0010636",
+      "name": "R0010636",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -0.877180253737663,
+        "pitch": 0.14317835588455097,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4680112355754327,
+          "pitch": 0.15274128345735782,
+          "rotation": 0,
+          "target": "3-r0010631"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.877180253737663,
+          "pitch": 0.14317835588455097,
+          "title": "Der Nachwuchs",
+          "text": "<span style=\"color: rgb(17, 17, 17); font-family: -apple-system, Roboto, SegoeUI, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Microsoft YaHei&quot;, &quot;Meiryo UI&quot;, Meiryo, &quot;Arial Unicode MS&quot;, sans-serif; font-size: 14px; background-color: rgb(243, 243, 243);\">Die Kälber auf dem Bauernhof sind die wahren Abenteurer und Entdecker. Mit ihren großen, neugierigen Augen und wackeligen Beinen erkunden sie mutig jede Ecke des Stalls und der Weide. Kein Grashalm bleibt unentdeckt, kein Zaunpfahl unbeschnuppert.</span>"
+        }
+      ]
+    },
+    {
+      "id": "3-r0010631",
+      "name": "R0010631",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -1.5818081664665105,
+        "pitch": 0.09386310394467756,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.564684067049175,
+          "pitch": 0.14110658535561527,
+          "rotation": 0,
+          "target": "4-r0010633"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5272144343637617,
+          "pitch": 0.4742148076311157,
+          "title": "<strong style=\"font-size: 14px; line-height: var(--cib-type-body1-stronger-line-height); font-variation-settings: var(--cib-type-body1-stronger-font-variation-settings); color: rgb(17, 17, 17); font-family: -apple-system, Roboto, SegoeUI, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Microsoft YaHei&quot;, &quot;Meiryo UI&quot;, Meiryo, &quot;Arial Unicode MS&quot;, sans-serif; background-color: rgb(243, 243, 243);\">Gesellschaft und Unterhaltung</strong>",
+          "text": "<span style=\"color: rgb(17, 17, 17); font-family: -apple-system, Roboto, SegoeUI, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Microsoft YaHei&quot;, &quot;Meiryo UI&quot;, Meiryo, &quot;Arial Unicode MS&quot;, sans-serif; font-size: 14px; background-color: rgb(243, 243, 243);\">Kühe sind soziale Tiere und genießen die Gesellschaft ihrer Herdenmitglieder. In einem gut gestalteten Stall können sie sich frei bewegen, miteinander kommunizieren und ihre sozialen Bindungen stärken.</span>"
+        }
+      ]
+    },
+    {
+      "id": "4-r0010633",
+      "name": "R0010633",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1680,
+      "initialViewParameters": {
+        "yaw": -2.5478771811047416,
+        "pitch": 0.1329628631182942,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.21878655789976,
+          "pitch": 0.11445871790735751,
+          "rotation": 0,
+          "target": "0-r0010634"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.332351403370877,
+          "pitch": 0.20545605472944217,
+          "title": "Erna",
+          "text": "<p style=\"word-break: break-word; font-size: 14px; line-height: var(--cib-type-body1-line-height); font-weight: 400; font-variation-settings: var(--cib-type-body1-font-variation-settings); color: rgb(17, 17, 17); font-family: -apple-system, Roboto, SegoeUI, &quot;Segoe UI&quot;, &quot;Helvetica Neue&quot;, Helvetica, &quot;Microsoft YaHei&quot;, &quot;Meiryo UI&quot;, Meiryo, &quot;Arial Unicode MS&quot;, sans-serif; font-style: normal; font-variant-ligatures: normal; font-variant-caps: normal; background-color: rgb(243, 243, 243);\">Erna ist das wahre Multitalent auf dem Bauernhof. Mit ihrem eleganten Gang und ihrem modischen Fleckenmuster könnte sie problemlos auf den Laufstegen von Paris und Mailand mithalten. Doch statt sich mit Haute Couture zu beschäftigen, widmet sie sich lieber der Kunst des Wiederkäuens – eine Fähigkeit, die sie zur wahren Meisterschaft gebracht hat.</p>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
